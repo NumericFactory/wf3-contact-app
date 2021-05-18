fb7b7577d029d88a2d821fdf3b5b96466fc318a9
NumericFactory <frederic.lossignol@gmail.com>
2021-05-18 14:41:36 +0200

    mise à jour

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -25,7 +25,6 @@ export class ContactService {
     const index = this.dbContacts.findIndex(contactInDb => contactInDb == contact); // 1
     // 2 set la propriété isFav à TRUE ou FALSE
     this.dbContacts[index].isFav = !this.dbContacts[index].isFav;
-    console.log(this.dbContacts[index].isFav);
     console.log(this.dbContacts)
   }
 
