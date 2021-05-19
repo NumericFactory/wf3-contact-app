8bf921086ec14d690cf1d4b7e53ff6b5d1ac3eb8
NumericFactory <frederic.lossignol@gmail.com>
2021-05-19 16:06:21 +0200

    mise à jour

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ContactService } from '../services/contact.service';
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponent implements OnInit {
 
-  constructor() { }
+  id;
+  // variable à afficher
+  contact;
+
+  /* On inject un objet de la class ActivatedRoute */
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private contactService: ContactService,
+  ) { }
 
   ngOnInit(): void {
+    // 1 récupérer l'ID dans l'url avec ActivatedRoute
+    console.log(this.activatedRoute.snapshot.params);
+    this.id = this.activatedRoute.snapshot.params.id; // {id: 1, name: 'fred lo'}
+
+    // 2 récupere le bon object contact dans dbContacts 
+    //   (dans le service)
+    this.contact = this.contactService.getContacts()[this.id];
+    console.log(this.contact);
+
+
   }
 
 }
