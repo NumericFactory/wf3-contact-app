6b2348beef9533ba1bb7ddd4e3057c061d63a149
NumericFactory <frederic.lossignol@gmail.com>
2021-05-19 12:15:14 +0200

    mise à jour

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailComponent } from './detail/detail.component';
+import { ListComponent } from './list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListComponent },
+  { path: 'detail', component: DetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
